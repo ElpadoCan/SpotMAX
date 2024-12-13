825724aee076f2e7d519d3e00861f43c187072c3
Francesco <padovaf@tcd.ie>
2024-12-13 17:11:50 +0100

    feat: test bioimageio model script

--- spotmax/BioImageIO/test_any_model_script.py
+++ spotmax/BioImageIO/test_any_model_script.py
@@ -2,17 +2,24 @@ import os
 
 import numpy as np
 
+from typing import Tuple
+
 import bioimageio.core
 from bioimageio.spec.model import AnyModelDescr
+from bioimageio.spec.model.v0_5 import TensorId
 
 from cellacdc.plot import imshow
 from spotmax import spotmax_path
 
 MODEL_SOURCE = os.path.join(
-    spotmax_path, 'BioImageIO', 'SpotMAX_UNet_2D', 'SpotMAX_AI_2D.zip'
+    spotmax_path, 'BioImageIO', 'example_models', 'SpotMAX_AI_2D', 'rdf.yaml'
 )
+# INPUT_IMAGE_PATH = os.path.join(
+#     spotmax_path, 'BioImageIO', 'SpotMAX_UNet_2D', 'input_sample.npy'
+# )
 INPUT_IMAGE_PATH = os.path.join(
-    spotmax_path, 'BioImageIO', 'SpotMAX_UNet_2D', 'input_sample.npy'
+    spotmax_path, 'BioImageIO', 'SpotMAX_UNet_2D', 'Input_sample_Positions',
+    'Position_11', 'Images', 'ASY15-1_60nM-11_s11_mNeon.tif'
 )
 
 def load_and_squeeze_input_image():
@@ -20,53 +27,93 @@ def load_and_squeeze_input_image():
         input_img = np.load(INPUT_IMAGE_PATH)
     elif INPUT_IMAGE_PATH.endswith('.tif'):
         import skimage.io
-        skimage.io.imread(INPUT_IMAGE_PATH)
+        input_img = skimage.io.imread(INPUT_IMAGE_PATH)
     elif INPUT_IMAGE_PATH.endswith('.npz'):
         input_img = np.load(INPUT_IMAGE_PATH)['arr_0']
     
     return np.squeeze(input_img)
 
-def reshape_input_image(input_image: np.ndarray, model_descr: AnyModelDescr):
+def create_input_sample(input_image: np.ndarray, model_descr: AnyModelDescr):
     axes = model_descr.inputs[0].axes
     space_axis_ids = {'z', 'y', 'x'}
+    output_index = []
     for axis in axes:
         if axis.id == 'z' and input_image.ndim == 2:
             input_image = input_image[np.newaxis]
+            output_index.append(0)
     
     for axis in axes:
         if axis.id in space_axis_ids:
             continue
         
         input_image = input_image[np.newaxis]
+        output_index.append(0)
     
-    return input_image
+    dims = [axis.id for axis in axes]
+    input_tensor = bioimageio.core.Tensor(
+        array=input_image, 
+        dims=dims
+    )
+    
+    input_ids = bioimageio.core.digest_spec.get_member_ids(
+        model_descr.inputs
+    )
+    
+    sample = bioimageio.core.Sample(
+        members={
+            TensorId(input_ids[0]): input_tensor
+        }, 
+        stat={}, 
+        id='inputs'
+    )
 
-def main():
+    return sample, tuple(output_index)
+
+def get_prediction_mask(
+        prediction_output: bioimageio.core.Sample, 
+        model_descr: AnyModelDescr,
+        output_index: Tuple[int]
+    ):
+    members = prediction_output.members
+    out_ids = list(members.keys())
+    out_id = out_ids[0]
+    prediction_mask = np.array(members[out_id])[output_index].astype(bool)
+    return prediction_mask
+
+def load_model_descr():
     print(f'Loading model description from "{MODEL_SOURCE}"...')
     model_descr = bioimageio.core.load_model_description(MODEL_SOURCE)
+    return model_descr
+
+def test_model():
+    model_descr = load_model_descr()
+    validation_summary = bioimageio.core.test_model(model_descr)
+
+def main():
+    model_descr = load_model_descr()
     
     print('Loading input image...')
     input_img = load_and_squeeze_input_image()
     
     print('Reshaping input image...')
-    input_img = reshape_input_image(input_img, model_descr)
+    sample, output_index = create_input_sample(input_img, model_descr)
     
     print('Creating prediction pipeline...')
     prediction_pipeline = bioimageio.core.create_prediction_pipeline(
         model_descr
     )
     
-    import pdb; pdb.set_trace()
-    
     print('Running prediction...')
-    predict_output = bioimageio.core.predict(
-        model=model_descr, 
-        inputs=input_img
+    prediction_output = prediction_pipeline.predict_sample_without_blocking(
+        sample
     )
-    import pdb; pdb.set_trace()
     
+    prediction_mask = get_prediction_mask(
+        prediction_output, model_descr, output_index
+    )
     
+    imshow(input_img, prediction_mask)  
     
-
 if __name__ == '__main__':
     main()
+    # main()
